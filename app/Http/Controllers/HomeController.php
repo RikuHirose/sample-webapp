67d49b70b88338be89355551b394d2f02c60e789
RikuHirose <rikuparkour9@gmail.com>
2020-08-29 21:03:36 +0900

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,8 @@ use Storage;
 
 use App\Jobs\Hoge;
 use App\Models\Job;
+use Aws\MediaConvert\MediaConvertClient;
+use Aws\Exception\AwsException;
 
 class HomeController extends Controller
 {
@@ -47,30 +49,77 @@ class HomeController extends Controller
             $originalPath      = $this->disk->putFileAs($folderName, $requestFile, $originalFileName, 'public');
             $originalUrl       = $this->disk->url($originalPath);
 
-            // convert HLS original video
-            $hlsPath = 'artist-files/movies/streaming/'.$requestFile->hashName().'.m3u8';
+        //     // convert HLS original video
+        //     $hlsPath = 'artist-files/movies/streaming/'.$requestFile->hashName().'.m3u8';
 
-            $lowBitrateFormat = (new X264('libmp3lame', 'libx264'))->setKiloBitrate(500);
+        //     $lowBitrateFormat = (new X264('libmp3lame', 'libx264'))->setKiloBitrate(500);
 
-            $hls = FFMpeg::fromDisk('s3')
-                ->open($originalPath)
-                ->exportForHLS()
-                ->addFormat($lowBitrateFormat)
-                ->save($hlsPath);
+        //     $hls = FFMpeg::fromDisk('s3')
+        //         ->open($originalPath)
+        //         ->exportForHLS()
+        //         ->addFormat($lowBitrateFormat)
+        //         ->save($hlsPath);
 
-            $hlsUrl = $this->disk->url($hlsPath);
+        //     $hlsUrl = $this->disk->url($hlsPath);
 
-            // get thumbnail url from original video
-            $originalThumbnailPath = 'artist-files/'.$requestFile->hashName().'.png';
-            FFMpeg::fromDisk('s3')
-              ->open($originalPath)
-              ->getFrameFromSeconds(1)
-              ->export()
-              ->toDisk('s3')
-              ->save($originalThumbnailPath);
-            $originalThumbnailUrl = $this->disk->url($originalThumbnailPath);
-            dd($originalThumbnailUrl);
+        //     // get thumbnail url from original video
+        //     $originalThumbnailPath = 'artist-files/'.$requestFile->hashName().'.png';
+        //     FFMpeg::fromDisk('s3')
+        //       ->open($originalPath)
+        //       ->getFrameFromSeconds(1)
+        //       ->export()
+        //       ->toDisk('s3')
+        //       ->save($originalThumbnailPath);
+        //     $originalThumbnailUrl = $this->disk->url($originalThumbnailPath);
+        //     dd($originalThumbnailUrl);
+        // }
+
+            // $client = new MediaConvertClient([
+            //     'profile' => 'default',
+            //     'version' => '2017-08-29',
+            //     'region' => config('filesystems.disks.s3.region')
+            // ]);
+
+            // //retrieve endpoint
+            // $result = $client->describeEndpoints([]);
+            // $single_endpoint_url = $result['Endpoints'][0]['Url'];
+            //     dd($single_endpoint_url);
+            // try {
+            //     $result = $client->describeEndpoints([]);
+            //     $single_endpoint_url = $result['Endpoints'][0]['Url'];
+            // } catch (AwsException $e) {
+            //     // output error message if fails
+            //     echo $e->getMessage();
+            //     echo "\n";
+            // }
+
+            $mediaConvertClient = new MediaConvertClient([
+                'version' => '2017-08-29',
+                'region' => config('filesystems.disks.s3.region'),
+                'profile' => 'default',
+                // 'endpoint' => $single_endpoint_url
+            ]);
+
+            $jobSetting = json_decode(file_get_contents(base_path('MediaConvert.json')), true);
+
+            try {
+                $result = $mediaConvertClient->createJob([
+                    "Role"     => "arn:aws:iam::818711851313:role/MediaConvert_Default_Role",
+                    "Settings" => $jobSetting, //JobSettings structure
+                    "Queue"    => "arn:aws:mediaconvert:ap-northeast-1:818711851313:queues/Default",
+                    // 'credentials' => [
+                    //        'key' => config('filesystems.disks.s3.key'),
+                    //        'secret' => config('filesystems.disks.s3.secret'),
+                    // ],
+                ]);
+                dd($result);
+            } catch (AwsException $e) {
+                // output error message if fails
+                echo $e->getMessage();
+                echo "\n";
+            }
         }
+
     }
 
     private function getFileName($requestFile, $fileExtension)
