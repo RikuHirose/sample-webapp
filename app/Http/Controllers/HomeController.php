8cbcb0c45465723828da7479ddc5be1aff03e3d6
RikuHirose <rikuparkour9@gmail.com>
2020-10-05 14:58:51 +0900

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,6 +36,7 @@ class HomeController extends Controller
     public function show(Request $request)
     {
         Hoge::dispatch()->delay(10);
+        
     }
 
     public function store(Request $request)
